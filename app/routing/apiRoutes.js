bb7ac6057921f0e5991612f48701dd06a74feb5e
liliana Quinley <lilicuellar99@gmail.com>
2018-11-20 00:45:18 -0700

    Set files servers and routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+
+      friendsData.push(req.body);
+      res.json(true);
+    
+  });
+
+//   // ---------------------------------------------------------------------------
+//   // I added this below code so you could clear out the table while working with the functionality.
+//   // Don"t worry about it!
+
+//   app.post("/api/clear", function(req, res) {
+//     // Empty out the arrays of data
+//     tableData.length = [];
+//     tableData.length = [];
+
+//     res.json({ ok: true });
+//   });
+};
