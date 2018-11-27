4ed3f77dfbe6df141b40b32767de3399feef33fc
liliana Quinley <lilicuellar99@gmail.com>
2018-11-26 22:41:49 -0700

    Created Match Function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,11 +17,37 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
+     
+  
+
+        // Take newScores and loop through existing scores, comparing each value and coming up wi
+      // a difference for each old score.
+    
+      var newScores = req.body.scores;
+      console.log(newScores)
+      console.log("Printing out friends data")
+
+      var matchResults = 0;
+      for (i=0; i< friendsData.length ; i++ ) {
+        for (x=0; x<newScores.length; x++) {
+          matchResults = matchResults + Math.abs(friendsData[i].scores[x] - newScores[x]);
+          console.log("Diff is " + matchResults)
+        }
+        console.log(req.body.scores)
+        console.log(friendsData[i].scores);
+  
+      }
 
       friendsData.push(req.body);
       res.json(true);
     
+
+
+
+    
   });
+
+
 
 //   // ---------------------------------------------------------------------------
 //   // I added this below code so you could clear out the table while working with the functionality.
